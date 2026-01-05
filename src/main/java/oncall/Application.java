8c4da94a71b9b7fc2128fa8ce0fdb2bb1888a73b
2jaeheon <jaeheon0826@jbnu.ac.kr>
2026-01-05 21:01:32 +0900

    fix: 복습을 위한 삭제
    Signed-off-by: 2jaeheon <jaeheon0826@jbnu.ac.kr>

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,10 +1,6 @@
 package oncall;
 
-import oncall.controller.Controller;
-
 public class Application {
     public static void main(String[] args) {
-        Controller controller = new Controller();
-        controller.run();
     }
 }
